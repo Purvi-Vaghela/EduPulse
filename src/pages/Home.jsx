b830d51d1883e6d871ed0d753c07f477086f6f03
PurviVaghela <purvi09v@gmail.com>
2024-12-14 16:29:38 +0530

    Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaArrowRight } from "react-icons/fa";
+import HighlightText from "../components/core/HomePage/HighlightText"
+import CTAButton from "../components/core/HomePage/Button"
+import Banner from "../assets/Images/banner.mp4"
+import CodeBlocks from "../components/core/HomePage/CodeBlocks"
+
+const Home = () => {
+  return (
+    <div className="bg-richblack-900 min-h-screen">
+      {/* Section 1 */}
+      <div className="relative mx-auto flex w-full max-w-screen-lg flex-col items-center gap-8 text-white">
+        <Link to={"/signup"}>
+          <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
+            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
+              <p>Become an Instructor</p>
+              <FaArrowRight />
+            </div>
+          </div>
+        </Link>
+
+        {/* Heading */}
+        <div className="text-center text-4xl font-semibold">
+          Empower Your Future with
+          <HighlightText text={"Coding Skills"} />
+        </div>
+
+        {/* Sub Heading */}
+        <div className="mt-4  w-full text-center text-xl font-bold text-richblack-100">
+          With our online coding courses, you can learn at your own pace, from
+          anywhere in the world, and get access to a wealth of resources,
+          including hands-on projects, quizzes, and personalized feedback from
+          instructors.
+        </div>
+
+        {/* CTA Buttons */}
+        <div className="w-full flex justify-center">
+          <div className="mt-8 flex flex-row gap-7 justify-center items-center">
+            <CTAButton active={true} linkto={"/signup"}>
+              Learn More
+            </CTAButton>
+            <CTAButton active={false} linkto={"/login"}>
+              Book a Demo
+            </CTAButton>
+          </div>
+        </div>
+
+          {/* Video */}
+          <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
+          <video
+            className="shadow-[20px_20px_rgba(255,255,255)]"
+            muted
+            loop
+            autoPlay
+          >
+            <source src={Banner} type="video/mp4" />
+          </video>
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
